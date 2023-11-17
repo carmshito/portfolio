9471b9681d10bc89d97f42f06dd2e6cf21d0feef
Carmelia Shito <carmshito@hotmail.com>
2023-11-17 00:04:24 -0500

    add side nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import TopNavBar from './components/TopNavBar';
+import SideNavBar from './components/SideNavBar';
 import AboutMe from './components/AboutMe';
 import MyProjects from './components/MyProjects';
 import ContactMe from './components/ContactMe';
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <main>
       <TopNavBar />
+      <SideNavBar />
       <AboutMe />
       <MyProjects />
       <ContactMe />
---
+++ src/components/SideNavBar.jsx
@@ -0,0 +1,33 @@
+import { Container } from "react-bootstrap";
+
+import "../styles/SideNavBar.scss";
+
+import AboutMeIcon from "../assets/about-me-icon.png";
+import MySkillsIcon from "../assets/my-skills-icon.png";
+import MyProjectsIcon from "../assets/my-projects-icon.png";
+import ContactMeIcon from "../assets/contact-me-icon.png";
+
+
+
+const SideNavBar = () => {
+  return (
+    <section className="side-nav-bar">
+      <Container className="icon-container">
+        <a href="" className="side-bar-icon">
+          <img src={AboutMeIcon} alt="About Me" />
+        </a>
+        <a href="" className="side-bar-icon">
+          <img src={MySkillsIcon} alt="My Skills" />
+        </a>
+        <a href="" className="side-bar-icon">
+          <img src={MyProjectsIcon} alt="My Projects" />
+        </a>
+        <a href="" className="side-bar-icon">
+          <img src={ContactMeIcon} alt="Contact Me" />
+        </a>
+      </Container>
+    </section>
+  );
+}
+
+export default SideNavBar;
