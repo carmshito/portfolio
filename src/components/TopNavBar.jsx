0f78b342ebca4cbbfcfabd879f1f036c1153c062
Carmelia Shito <carmshito@hotmail.com>
2023-11-16 22:26:25 -0500

    fix TopNavBar

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -6,17 +6,20 @@ import CactusSvg from "../assets/CactusSvg";
 import CVIconSvg from "../assets/CVIconSvg";
 import LinkedInIconSvg from "../assets/LinkedInIconSvg";
 import GitHubIconSvg from "../assets/GitHubIconSvg";
+import WifiSvg from "../assets/WifiSvg";
+import BatterySvg from "../assets/BatterySvg";
 
 const TopNavBar = () => {
   return (
       <Navbar className="top-nav-bar">
-        <Container>
+        <Container className="container">
           <div className="nav-bar-text">
             <Navbar.Brand href="#home"><CactusSvg /></Navbar.Brand>
             <span className="nav-bar-name">Carmelia Shito</span>
           </div>
           <div>
-            <span>carmshito@gmail.com</span>
+            <BatterySvg />
+            <WifiSvg />
             <a href="#"><CVIconSvg /></a>
             <a href="https://www.linkedin.com/in/carmelia-shito/"><LinkedInIconSvg /></a>
             <a href="https://github.com/carmshito"><GitHubIconSvg /></a>
