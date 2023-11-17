f1cdaffe569091c734bd2e61750c6d2db732d628
Carmelia Shito <carmshito@hotmail.com>
2023-11-17 14:41:07 -0500

    add about me text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,6 +12,13 @@ const AboutMe = () => {
           AboutMe.txt
         </span>
       </div>
+      <h1 className="hello-text">Hello!</h1>
+      <p className="about-me-text">
+      My name is <b>Carmelia</b>, but I also like to go by <b>Carm</b>. I am a <b>full stack web developer</b> and <b>creative</b> based in <b>Toronto</b>.<br/><br/>
+      This portfolio is my playground, featuring my projects and illustrations.<br/>
+      I enjoy drawing, playing sports, meeting new people, and taking on new challenges.<br/>
+      Let's create something amazing together!
+      </p>
     </Container>
   );
 }
