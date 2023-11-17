b789cdaac32bf231ce587486ab2ef47cd5a699bf
Carmelia Shito <carmshito@hotmail.com>
2023-11-16 23:12:04 -0500

    add background image

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Container } from "react-bootstrap";
 
-import '../styles/TopNavBar.scss';
+import "../styles/TopNavBar.scss";
 
 import CactusSvg from "../assets/CactusSvg";
 import CVIconSvg from "../assets/CVIconSvg";
@@ -11,22 +11,30 @@ import BatterySvg from "../assets/BatterySvg";
 
 const TopNavBar = () => {
   return (
-      <Navbar className="top-nav-bar">
-        <Container className="container">
-          <div className="nav-bar-text">
-            <Navbar.Brand href="#home"><CactusSvg /></Navbar.Brand>
-            <span className="nav-bar-name">Carmelia Shito</span>
-          </div>
-          <div>
-            <BatterySvg />
-            <WifiSvg />
-            <a href="#"><CVIconSvg /></a>
-            <a href="https://www.linkedin.com/in/carmelia-shito/"><LinkedInIconSvg /></a>
-            <a href="https://github.com/carmshito"><GitHubIconSvg /></a>
-          </div>
-        </Container>
-      </Navbar>
-  );   
-}
+    <Navbar className="top-nav-bar">
+      <Container className="container">
+        <div className="nav-bar-text">
+          <Navbar.Brand href="#home">
+            <CactusSvg />
+          </Navbar.Brand>
+          <span className="nav-bar-name">Carmelia Shito</span>
+        </div>
+        <div>
+          <BatterySvg />
+          <WifiSvg />
+          <a href="#">
+            <CVIconSvg />
+          </a>
+          <a href="https://www.linkedin.com/in/carmelia-shito/">
+            <LinkedInIconSvg />
+          </a>
+          <a href="https://github.com/carmshito">
+            <GitHubIconSvg />
+          </a>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default TopNavBar;
